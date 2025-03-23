c444d70574330baa1b69d48bafa494dd1369d41a
minseo Jo <liging12@naver.com>
2025-03-23 14:51:36 +0900

    init Spring Boot backend and basic Three.js setup without build tools

---
+++ backend/src/test/java/com/minseojo/smartwarehouse/SmartwarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minseojo.smartwarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartwarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
